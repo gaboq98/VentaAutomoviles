d9a6330a04a400f1feae165e3825f0e24254cae5
Unknown <juandibp@gmail.com>
2019-06-17 23:31:21 -0600

    CRUDS

---
+++ VentaAutomoviles/Controllers/ConsignacionesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VentaAutomoviles.Models;
+
+namespace VentaAutomoviles.Controllers
+{
+    public class ConsignacionesController : Controller
+    {
+        private VentaAutomovilesEntities db = new VentaAutomovilesEntities();
+
+        // GET: Consignaciones
+        public ActionResult Index()
+        {
+            var consignacion = db.Consignacion.Include(c => c.Automovil).Include(c => c.Sucursal);
+            return View(consignacion.ToList());
+        }
+
+        // GET: Consignaciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consignacion consignacion = db.Consignacion.Find(id);
+            if (consignacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consignacion);
+        }
+
+        // GET: Consignaciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAutomovil = new SelectList(db.Automovil, "IdAutomovil", "IdAutomovil");
+            ViewBag.IdSucursal = new SelectList(db.Sucursal, "IdSucursal", "Nombre");
+            return View();
+        }
+
+        // POST: Consignaciones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdConsignacion,IdSucursal,IdAutomovil,Comision")] Consignacion consignacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consignacion.Add(consignacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAutomovil = new SelectList(db.Automovil, "IdAutomovil", "IdAutomovil", consignacion.IdAutomovil);
+            ViewBag.IdSucursal = new SelectList(db.Sucursal, "IdSucursal", "Nombre", consignacion.IdSucursal);
+            return View(consignacion);
+        }
+
+        // GET: Consignaciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consignacion consignacion = db.Consignacion.Find(id);
+            if (consignacion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAutomovil = new SelectList(db.Automovil, "IdAutomovil", "IdAutomovil", consignacion.IdAutomovil);
+            ViewBag.IdSucursal = new SelectList(db.Sucursal, "IdSucursal", "Nombre", consignacion.IdSucursal);
+            return View(consignacion);
+        }
+
+        // POST: Consignaciones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdConsignacion,IdSucursal,IdAutomovil,Comision")] Consignacion consignacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consignacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAutomovil = new SelectList(db.Automovil, "IdAutomovil", "IdAutomovil", consignacion.IdAutomovil);
+            ViewBag.IdSucursal = new SelectList(db.Sucursal, "IdSucursal", "Nombre", consignacion.IdSucursal);
+            return View(consignacion);
+        }
+
+        // GET: Consignaciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consignacion consignacion = db.Consignacion.Find(id);
+            if (consignacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consignacion);
+        }
+
+        // POST: Consignaciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Consignacion consignacion = db.Consignacion.Find(id);
+            db.Consignacion.Remove(consignacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VentaAutomoviles/Controllers/ExtrasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VentaAutomoviles.Models;
+
+namespace VentaAutomoviles.Controllers
+{
+    public class ExtrasController : Controller
+    {
+        private VentaAutomovilesEntities db = new VentaAutomovilesEntities();
+
+        // GET: Extras
+        public ActionResult Index()
+        {
+            return View(db.Extra.ToList());
+        }
+
+        // GET: Extras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extra extra = db.Extra.Find(id);
+            if (extra == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extra);
+        }
+
+        // GET: Extras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Extras/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdExtra,Nombre,PrecioUnitario")] Extra extra)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Extra.Add(extra);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(extra);
+        }
+
+        // GET: Extras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extra extra = db.Extra.Find(id);
+            if (extra == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extra);
+        }
+
+        // POST: Extras/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdExtra,Nombre,PrecioUnitario")] Extra extra)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(extra).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(extra);
+        }
+
+        // GET: Extras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Extra extra = db.Extra.Find(id);
+            if (extra == null)
+            {
+                return HttpNotFound();
+            }
+            return View(extra);
+        }
+
+        // POST: Extras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Extra extra = db.Extra.Find(id);
+            db.Extra.Remove(extra);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
